cf4811ed65566825a0c3bfe597580dfc7c95467b
Julian <jdbosca@komatsu.com.au>
2024-10-20 08:57:56 +0000

    -

--- pages/Contracts/jsobjects/onRowSelect/onRowSelect.js
+++ pages/Contracts/jsobjects/onRowSelect/onRowSelect.js
@@ -1,6 +1,6 @@
 export default {
   onRowSelect: () => {
-    return storeValue('asset_id', data_table.selectedRow.smartcare_subscription_id)
+    return storeValue('subscription_id', data_table.selectedRow.smartcare_subscription_id)
       .then(() => {
         navigateTo('Machine Contract Details');
       });
